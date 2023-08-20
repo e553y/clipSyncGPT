1e0bc0cd13453a842df97e381de0405aa909fece
e553y <esey.iyasu@gmail.com>
2023-08-19 22:14:58 -0700

    feat: add go client for Mac

---
+++ clients/go/clipSyncClient.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"io"
+	"strings"
+	"fmt"
+	"log"
+	"os/exec"
+	"time"
+	"github.com/gorilla/websocket"
+)
+
+var addr = "192.168.1.76:8080"
+var reconnectDelay = 5 * time.Second
+
+func getClipboard() string {
+	out, err := exec.Command("osascript", "-e", "get the clipboard as string").Output()
+	if err != nil {
+		log.Println("Warning: Unable to get clipboard content:", err)
+		return ""
+	}
+	return strings.TrimSpace(string(out))
+}
+
+func setClipboard(data string) {
+	cmd := exec.Command("pbcopy")
+	stdin, err := cmd.StdinPipe()
+	if err != nil {
+		log.Fatal(err)
+	}
+	go func() {
+		defer stdin.Close()
+		io.WriteString(stdin, data)
+	}()
+	err = cmd.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func connectToServer() *websocket.Conn {
+	c, _, err := websocket.DefaultDialer.Dial("ws://"+addr, nil)
+	if err != nil {
+		log.Println("Failed to connect:", err)
+		time.Sleep(reconnectDelay)
+		return connectToServer()
+	}
+	log.Println("Successfully connected to the server!")
+	return c
+}
+
+func main() {
+	c := connectToServer()
+	defer c.Close()
+	lastClipboard := ""
+
+	go func() {
+		for {
+			_, message, err := c.ReadMessage()
+			if err != nil {
+				log.Println("Connection lost. Attempting to reconnect...")
+				c = connectToServer()
+				continue
+			}
+			fmt.Println("Received:", string(message))
+			lastClipboard = strings.TrimSpace(string(message))
+			setClipboard(lastClipboard)
+		}
+	}()
+
+	for {
+		currentClipboard := getClipboard()
+		if currentClipboard != lastClipboard {
+			fmt.Println("Sending:", string(currentClipboard))
+			err := c.WriteMessage(websocket.TextMessage, []byte(currentClipboard))
+			if err != nil {
+				log.Println("Failed to write message:", err)
+				time.Sleep(reconnectDelay)
+				c = connectToServer()
+				continue
+			}
+			lastClipboard = currentClipboard
+		}
+		time.Sleep(1 * time.Second)
+	}
+}
