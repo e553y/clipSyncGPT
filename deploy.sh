15b5ea75feec85cb682a124ec0192db70cda2705
e553y <esey.iyasu@gmail.com>
2023-08-24 21:17:19 -0700

    deployment: again

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,15 @@
-#!/bin/bash
+# Install NVM (Node Version Manager)
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
 
+# Activate NVM
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+# Install and use Node.js 18.x
+nvm install 18
+nvm use 18
+
+# Rest of your script
 cd ~/server
 echo pwd:`pwd`
 echo node:`node --version`
