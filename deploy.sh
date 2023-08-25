0e23ecfd1968d9d6a89bcb5c754372d24ee3eb3c
e553y <esey.iyasu@gmail.com>
2023-08-24 20:36:24 -0700

    update start command

--- deploy.sh
+++ deploy.sh
@@ -6,5 +6,5 @@ pm2 ls
 if pm2 info clip-sync-server > /dev/null 2>&1; then 
   pm2 restart clip-sync-server --update-env; 
 else 
-  pm2 start index.js --name clip-sync-server; 
+  pm2 start "node ./index.js" --name clip-sync-server; 
 fi
