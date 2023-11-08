583e7319e63bd9bc36cce41dce7960f2148183de
e553y <esey.iyasu@gmail.com>
2023-11-07 20:24:57 -0800

    fix: update deploy script to enable listening on port 80

--- deploy.sh
+++ deploy.sh
@@ -11,11 +11,17 @@ nvm use 18
 
 # Rest of your script
 cd ~/server
-echo pwd:`pwd`
-echo node:`node --version`
+
+# Output the current directory and node version for debugging
+echo "pwd: $(pwd)"
+echo "node: $(node --version)"
+
+# Check if PM2 is running and list all applications
 pm2 ls
+
+# Check if clip-sync-server is already running, restart it if so, otherwise start it
 if pm2 info clip-sync-server > /dev/null 2>&1; then 
   pm2 restart clip-sync-server --update-env; 
 else 
-  pm2 start "node ./index.js" --name clip-sync-server; 
+  pm2 start "authbind --deep node ./index.js" --name clip-sync-server; 
 fi
